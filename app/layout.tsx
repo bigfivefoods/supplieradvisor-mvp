3b600d12711cdfb9a9736df948ad8b5e73792d58
bigfivefoods <craig@bigfivefoods.com>
2026-03-26 01:00:41 +0000

    fix layout.tsx – added missing PrivyProvider wrapper + full sidebar

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,6 +2,7 @@
 
 import { useState } from 'react';
 import { PrivyProvider } from '@privy-io/react-auth';
+import { usePrivy } from '@privy-io/react-auth';
 import Link from 'next/link';
 import { usePathname } from 'next/navigation';
 import Image from 'next/image';
