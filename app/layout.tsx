217644dd1605be2867524bbaef8e0649c6237b93
bigfivefoods <craig@bigfivefoods.com>
2026-06-15 04:17:44 +0000

    Clean layouts - fixed createContext error

--- app/layout.tsx
+++ app/layout.tsx
@@ -6,9 +6,7 @@ import { Toaster } from "react-hot-toast";
 export const metadata: Metadata = {
   title: 'SupplierAdvisor',
   description: 'Business Network for Africa',
-  icons: {
-    icon: '/sa-logo.png',
-  },
+  icons: { icon: '/sa-logo.png' },
 };
 
 export default function RootLayout({ children }: { children: React.ReactNode }) {
@@ -16,7 +14,6 @@ export default function RootLayout({ children }: { children: React.ReactNode }) 
     <html lang="en">
       <head>
         <link rel="icon" href="/sa-logo.png" type="image/png" />
-        {/* Paystack Script */}
         <script src="https://js.paystack.co/v1/inline.js" />
       </head>
       <body className="antialiased">
