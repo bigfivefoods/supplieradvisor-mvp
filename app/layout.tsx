1d72bd56ef9ba4415b214d3f355cf66cf343234f
bigfivefoods <craig@bigfivefoods.com>
2026-06-16 08:11:04 +0000

    Add Paystack script to layout

--- app/layout.tsx
+++ app/layout.tsx
@@ -9,6 +9,9 @@ export default function RootLayout({ children }: { children: React.ReactNode }) 
     <html lang="en">
       <head>
         <link rel="icon" href="/sa-logo.png" type="image/png" />
+        
+        {/* Paystack Inline Script - Added for verification payments */}
+        <script src="https://js.paystack.co/v1/inline.js" async />
       </head>
       <body className="antialiased">
         <PrivyProvider
